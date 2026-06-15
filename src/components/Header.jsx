032ce18d19b0d7d2ea7f5ad308a9c12ea4bf3f29
RosaBen <benchabane.rosa@outlook.com>
2026-06-15 20:37:25 +0200

    added style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ return (
     <header>
     <h1>{title}</h1>
     <p>{name}</p>
-    <div>
+    <div className="contact">
       <span><a href="tel:+33642228582">📞 {tel}</a></span>
       <span><a href="mailto:benchabane.rosa@outlook.com">✉ {email}</a></span>
-      <span><a href={portfolio}>🌐 Portfolio</a></span>
-      <span>📍 {location}</span>
+      <span><a href={portfolio} target="_blank">🌐 Portfolio</a></span>
+      <span><a href="https://maps.app.goo.gl/vGa6jdgRD7Qc1hsx5" target="_blank">📍 {location}</a></span>
     </div>
   </header>
 )
